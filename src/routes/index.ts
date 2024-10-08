d250265d9f417ac44b1b38e0b5114ee1e3f56b20
BEATRIZ DA SILVA CRUZ <beatriz.scruz7@senacsp.edu.br>
2024-10-08 16:45:04 -0300

    aula20

---
+++ src/routes/index.ts
@@ -0,0 +1,40 @@
+import { Router, Request, Response, RequestHandler } from 'express';
+
+const router = Router()
+
+router.get('/', (req: Request, res: Response) => {
+    res.send("Página home")
+})
+
+//quando usamos middleware, usamos NEXT
+const interferir: RequestHandler = (req,res,next) =>{
+    //console.log("PASSOU PELO MIDDLEWARE")
+    //next significa que liberamos o middleware a pag d. contatos
+    let logged = false
+    if(logged){
+        next()
+    }else{
+        res.status(404).send("Loggin não permitido")
+    } 
+}
+
+router.get('/contatos',interferir, (req: Request, res: Response) => {
+    //console.log("Executou a pagina de CONTATOS")
+    res.send("Página contatos")
+})
+router.get('/sobre', (req: Request, res: Response) => {
+    res.send("Página sobre")
+})
+
+
+//rota dinâmica
+router.get('/noticia/:slug',(req: Request,res: Response) => {
+    let slug: string = req.params.slug
+    res.send(`Noticia: ${slug}`)
+})
+router.get('/voo/:origem-:destino',(req: Request,res: Response) => {
+    let{origem, destino} = req.params
+    res.send(`Procurando voos de ${origem} até ${destino}`)
+})
+
+export default router
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,24 @@
 //importando o express 
-import express from 'express'
-import { Server } from 'http'
+import express, {Request, Response} from 'express'
+
+//importanto o arquivo index.ts das rotas
+import mainRoutes from './routes/index'
 
 //usando express 
 const server = express()
+
+//inserindo a rota no servidor
+server.use(mainRoutes)
+
+//página não encontrada
+server.use((req: Request, res: Response) => {
+    res.status(404).send("Página não encontrada")
+})
+
+//reg - Request
+//res - Response
+//get para só exibir --- post precisa de login
+
 
 //gerar o servidor na porta 3000
 server.listen(3000)
