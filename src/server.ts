540602a4e7e0d27c76d95e84a0a7081892c0c19d
BEATRIZ DA SILVA CRUZ <beatriz.scruz7@senacsp.edu.br>
2024-10-08 17:19:15 -0300

    aula20

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,15 @@
 //importando o express 
 import express, {Request, Response} from 'express'
+import path from 'path'
 
 //importanto o arquivo index.ts das rotas
 import mainRoutes from './routes/index'
 
 //usando express 
 const server = express()
+
+//configuração da pasta public
+server.use(express.static(path.join(__dirname,'../public')))
 
 //inserindo a rota no servidor
 server.use(mainRoutes)
